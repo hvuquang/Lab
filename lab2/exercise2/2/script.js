49d591a2c6680766b8e5251f1069cb714029e553
Numb246 <113155975+hvuquang@users.noreply.github.com>
2022-10-12 10:26:07 +0700

    ex2.2

---
+++ lab2/exercise2/2/script.js
@@ -0,0 +1,6 @@
+function changeBackGroundText() {
+    var bg = document.getElementById("body");
+    bg.style.backgroundColor = "lightblue";
+    var txt = document.getElementsByTagName("p");
+    bg.style.color = "purple";
+}
