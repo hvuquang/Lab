87e383cfb68268e799a331cc1766c06ad37491e4
Numb246 <113155975+hvuquang@users.noreply.github.com>
2022-10-24 22:17:30 +0700

    update 5.2

--- lab2/exercise5/5.2/script.js
+++ lab2/exercise5/5.2/script.js
@@ -7,8 +7,8 @@ const secondHand = document.querySelector('[data-second-hand]')
 function setClock() {
     const currentDate = new Date();
     const secondsRatio = currentDate.getSeconds() / 60;
-    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
-    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+    const minutesRatio = ( secondsRatio + currentDate.getMinutes()) / 60;
+    const hoursRatio = ( minutesRatio + currentDate.getHours()) / 12;
 
     setRotation(secondHand, secondsRatio);
     setRotation(minuteHand, minutesRatio);
